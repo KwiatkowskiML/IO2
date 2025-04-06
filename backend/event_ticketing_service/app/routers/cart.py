597435bc29c62506d64ea2f598f2be99b737bf7d
kwiatkowskim <kwiatkowski.michal.2003@gmail.com>
2025-04-06 16:16:57 +0200

    Cart service

--- backend/event_ticketing_service/app/routers/cart.py
+++ backend/event_ticketing_service/app/routers/cart.py
@@ -1,30 +1,37 @@
 from typing import List
-
 from fastapi import Path, Depends, APIRouter
 
 from common.security import get_current_user
 from common.schemas.payment import PaymentResponse
-from common.schemas.ticket import TicketBase
+from common.schemas.ticket import TicketBase, TicketDetails
 
 router = APIRouter(prefix="/cart", tags=["cart"])
 
 @router.get("/items", response_model=List[TicketBase])
 async def get_shopping_cart(
-    #current_user=Depends(get_current_user)
+    current_user=Depends(get_current_user)
 ):
     return [TicketBase(
         ticket_id=1,
         ticket_type_id=1
     )]
 
-@router.post("/items")
-async def add_to_cart():
-    return "ok"
+@router.post("/items", response_model=bool)
+async def add_to_cart(
+    item: TicketDetails,
+    current_user=Depends(get_current_user)
+):
+    return True
 
-@router.delete("/items/{item_id}")
-async def remove_from_cart():
-    return "ok"
+@router.delete("/items/{ticket_id}", response_model=bool)
+async def remove_from_cart(
+    ticket_id: int = Path(..., title="Ticket ID", ge=1),
+    current_user=Depends(get_current_user)
+):
+    return True
 
-@router.post("/checkout")
-async def checkout_cart():
-    return "ok"
+@router.post("/checkout", response_model=bool)
+async def checkout_cart(
+    current_user=Depends(get_current_user)
+):
+    return True
